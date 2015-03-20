f16e42ad813c8ea603b82de8d188c227e3d1fe68
Didier Landry <didier.landry@gmail.com>
2015-03-19 22:49:10 -0400

    Added a rudimentary login. Figured out this maven bullcrap.

--- src/main/java/ca/utoronto/ece1779h/authentication/Authentication.java
+++ src/main/java/ca/utoronto/ece1779h/authentication/Authentication.java
@@ -1,4 +1,33 @@
 package ca.utoronto.ece1779h.authentication;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class Authentication {
+
+	public static User authenticate(){
+
+		UserService userService = UserServiceFactory.getUserService();
+
+        return userService.getCurrentUser();
+	}
+
+	public static String getLoginBar(){
+		String loginBar;
+
+		UserService userService = UserServiceFactory.getUserService();
+        User user =  userService.getCurrentUser();
+
+		if (user != null) {
+            loginBar = "<p>Welcome, " + user.getNickname() + "! You can <a href=\"" +
+                     userService.createLogoutURL("/") +
+                     "\">sign out</a>.</p>";
+        } else {
+            loginBar = "<p>Welcome! <a href=\"" + userService.createLoginURL("/") +
+                     "\">Sign in or register</a> to customize.</p>";
+        }
+
+        return loginBar;
+	}
 }
--- src/main/java/ca/utoronto/ece1779h/servlet/Hello.java
+++ src/main/java/ca/utoronto/ece1779h/servlet/Hello.java
@@ -4,12 +4,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import ca.utoronto.ece1779h.authentication.Authentication;
 
 public class Hello extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
+
+        User user = Authentication.authenticate();
+
+        String loginBar = Authentication.getLoginBar();
+
+        resp.setContentType("text/html");
+        resp.getWriter().println(loginBar);
         resp.getWriter().println("Hello, this is a testing servlet.");
+
     }
 }
