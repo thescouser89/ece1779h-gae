23c64fea0939b4424e08b75f926038b8348daf8b
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-03-30 17:18:51 -0400

    Adding more methods to models

--- src/main/java/ca/utoronto/ece1779h/model/Flashcard.java
+++ src/main/java/ca/utoronto/ece1779h/model/Flashcard.java
@@ -5,6 +5,7 @@ import com.google.appengine.api.datastore.Key;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.lang.Override;
+import javax.persistence.Column;
 
 @Entity
 public class Flashcard implements Serializable {
@@ -18,15 +19,37 @@ public class Flashcard implements Serializable {
     @Column(length = 10000)
     private String answer;
 
-    public Key getStackCard() {
-        return stackCard;
+    @Column
+    private int numberRights;
+
+    @Column
+    private int numberWrongs;
+
+    public Key getStackcardKey() {
+        return stackcard;
     }
 
-    public void setStackCard(Key stackCard) {
-        this.stackCard = stackCard;
+    public void setStackcardKey(Key stackcard) {
+        this.stackcard = stackcard;
     }
 
-    private Key stackCard;
+    /**
+     * Warning: not tested! ping Dustin if that doesn't work
+     * @return
+     */
+    public Stackcard stackcard() {
+        EntityManager em = EMF.get().createEntityManager();
+        try {
+            Query q = em.createQuery("find stackcard from Stackcard stackcard where stackcard.key = :key");
+            return (Stackcard) q.setParameter("key", getStackcardKey()).getSingleResult();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    @Column(nullable = false, updatable = false)
+    private Key stackcard;
 
     public Key getKey() {
         return this.key;
@@ -65,18 +88,6 @@ public class Flashcard implements Serializable {
         this.answer = answer;
     }
 
-    @Override
-    public String toString() {
-        String result = getClass().getSimpleName() + " ";
-        if (key != null)
-            result += "key: " + key;
-        if (question != null && !question.trim().isEmpty())
-            result += ", question: " + question;
-        if (answer != null && !answer.trim().isEmpty())
-            result += ", answer: " + answer;
-        return result;
-    }
-
     public void save() {
         EntityManager em = EMF.get().createEntityManager();
         try {
@@ -92,5 +103,37 @@ public class Flashcard implements Serializable {
         int result = 1;
         result = prime * result + ((question == null) ? 0 : question.hashCode());
         return result;
+    }
+
+    public int getNumberRights() {
+        return numberRights;
+    }
+
+    public int getNumberWrongs() {
+        return numberWrongs;
+    }
+
+    @Override
+    public String toString() {
+        String result = getClass().getSimpleName() + " ";
+        if (question != null && !question.trim().isEmpty())
+            result += "question: " + question;
+        if (answer != null && !answer.trim().isEmpty())
+            result += ", answer: " + answer;
+        result += ", numberRights: " + numberRights;
+        result += ", numberWrongs: " + numberWrongs;
+        return result;
+    }
+
+    public int incrementRights() {
+        numberRights++;
+        save();
+        return numberRights;
+    }
+
+    public int incrementWrongs() {
+        numberWrongs++;
+        save();
+        return numberWrongs;
     }
 }
---
+++ src/main/java/ca/utoronto/ece1779h/model/Stackcard.java
@@ -0,0 +1,129 @@
+package ca.utoronto.ece1779h.model;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.lang.Override;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+
+@Entity
+public class Stackcard implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Key key;
+
+    @Column
+    private String name;
+
+    @Column(length = 10000, nullable = false, updatable = false)
+    private String owner;
+
+    public static List<Stackcard> getStacks(String owner) {
+        EntityManager em = EMF.get().createEntityManager();
+        try {
+            List<Stackcard> stacks;
+            Query q = em.createQuery("select stackcard from Stackcard stackcard where stackcard.owner = :owner");
+            stacks = new ArrayList<Stackcard>(q.setParameter("owner", owner).getResultList());
+            return stacks;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * We know which flashcard belong to a stack because each flashcard must
+     * a key parameter that says to which stackcard it belongs.
+     * @return
+     */
+    public List<Flashcard> getFlashcards() {
+        EntityManager em = EMF.get().createEntityManager();
+        try {
+            List<Flashcard> flashcards;
+            Query q = em.createQuery("select flash from Flashcard flash where flash.stackcard = :key");
+            flashcards = new ArrayList<Flashcard>(q.setParameter("key", this.getKey()).getResultList());
+            return flashcards;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Flashcard newFlashcard() {
+        Flashcard flash = new Flashcard();
+        flash.setStackcardKey(this.getKey());
+        return flash;
+    }
+
+    public Key getKey() {
+        return this.key;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Stackcard)) {
+            return false;
+        }
+        Stackcard other = (Stackcard) obj;
+        if (key != null) {
+            if (!key.equals(other.key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    public void save() {
+        EntityManager em = EMF.get().createEntityManager();
+        try {
+            em.merge(this);
+        } finally {
+            em.close();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        String result = getClass().getSimpleName() + " ";
+        if (name != null && !name.trim().isEmpty())
+            result += "name: " + name;
+        if (owner != null && !owner.trim().isEmpty())
+            result += ", owner: " + owner;
+        return result;
+    }
+
+    // to implement
+    public void getStats() {};
+    public void clearStats() {};
+}
